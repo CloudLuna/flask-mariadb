a0deafd75cdc8fa89a39212ae108fdcd74664a7a
CloudLuna <constanza.lorca.luna@gmail.com>
2025-05-16 15:27:30 +1000

    changes app.py

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ from flask import Flask
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
-import mariadb
-import sys
+#import mariadb
+#import sys
 
 #DB setting
-def get_db():
+#def get_db():
 ##Connect with db
 #    try:
 #        conn = mariadb.connect(
@@ -23,8 +23,8 @@ def get_db():
 #   db = conn.cursor()
 #   return db
 
-def close_db():
-    conn.close()
+#def close_db():
+#    conn.close()
 
 app = Flask(__name__)
 
@@ -32,7 +32,7 @@ app = Flask(__name__)
 def homepage():
     return "Hello World"
 
-@app.route('/register', methods=('POST'))
+@app.route('/register', methods=('GET','POST'))
 def register():
     if request.method == 'POST':
         username = request.form['username']
@@ -46,16 +46,19 @@ def register():
             error = 'Password is required.'
 
         if error is None:
-            try:
-                db.execute(
-                    "INSERT INTO users (username, password) VALUES (?, ?)",
-                    (username, generate_password_hash(password)),
-                )
-            except mariadb.Error as e:
-                print(f"Error: {e}")
-                error = f"User {username} is already registered."
-            else:
-                print("Successful registration")
-                return redirect(url_for("hello"))
+#            try:
+#                db.execute(
+#                    "INSERT INTO users (username, password) VALUES (?, ?)",
+#                    (username, generate_password_hash(password)),
+#                )
+#            except mariadb.Error as e:
+#                print(f"Error: {e}")
+#                error = f"User {username} is already registered."
+#            else:
+#                print("Successful registration")
+#                return redirect(url_for("hello"))
+            return redirect("/hello")
+        flash(error)
 
-    return render_template('register.html')        
+
+    return render_template('register.html')
